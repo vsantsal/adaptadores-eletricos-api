905e1104333fe1ebc89d31c888dd1751b1748fb5
vsantsal <vsantsal@gmail.com>
2023-08-26 16:40:45 -0300

    feat: primeira impl funcional de cadastro de pessoas

--- src/main/java/com/example/adaptadoreseletricos/domain/entity/pessoa/ParentescoPessoas.java
+++ src/main/java/com/example/adaptadoreseletricos/domain/entity/pessoa/ParentescoPessoas.java
@@ -1,22 +1,30 @@
 package com.example.adaptadoreseletricos.domain.entity.pessoa;
 
 import jakarta.persistence.*;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Table(name = "parentesco_pessoas")
+@NoArgsConstructor
 public class ParentescoPessoas {
     @EmbeddedId
     ParentescoPessoasChave id;
 
-    @ManyToOne
-    @MapsId("pessoaId")
-    @JoinColumn(name = "id_pessoa1")
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "id_pessoa1", nullable = false, insertable = false, updatable = false)
     Pessoa pessoa1;
 
-    @ManyToOne
-    @MapsId("pessoaId")
-    @JoinColumn(name = "id_pessoa2")
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "id_pessoa2", nullable = false, insertable = false, updatable = false)
     Pessoa pessoa2;
 
     @Enumerated(EnumType.STRING)
     private Parentesco parentesco;
+
+    public ParentescoPessoas(Pessoa pessoa1, Pessoa pessoa2, Parentesco parentesco) {
+        this.id = new ParentescoPessoasChave(pessoa1, pessoa2);
+        this.pessoa1 = pessoa1;
+        this.pessoa2 = pessoa2;
+        this.parentesco = parentesco;
+    }
 }
--- src/main/java/com/example/adaptadoreseletricos/service/pessoa/PessoaService.java
+++ src/main/java/com/example/adaptadoreseletricos/service/pessoa/PessoaService.java
@@ -1,10 +1,15 @@
 package com.example.adaptadoreseletricos.service.pessoa;
 
+import com.example.adaptadoreseletricos.domain.entity.pessoa.Parentesco;
+import com.example.adaptadoreseletricos.domain.entity.pessoa.ParentescoPessoas;
 import com.example.adaptadoreseletricos.domain.entity.pessoa.Pessoa;
+import com.example.adaptadoreseletricos.domain.entity.pessoa.Usuario;
+import com.example.adaptadoreseletricos.domain.repository.pessoa.ParentescoPessoasRepository;
 import com.example.adaptadoreseletricos.domain.repository.pessoa.PessoaRepository;
 import com.example.adaptadoreseletricos.dto.pessoa.PessoaCadastroDTO;
 import com.example.adaptadoreseletricos.dto.pessoa.PessoaDetalheDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,17 +17,43 @@ import org.springframework.transaction.annotation.Transactional;
 public class PessoaService {
 
     @Autowired
-    private PessoaRepository repository;
+    private PessoaRepository pessoaRepository;
+
+    @Autowired
+    private ParentescoPessoasRepository parentescoPessoasRepository;
 
     @Transactional
     public PessoaDetalheDTO salvar(PessoaCadastroDTO dto) {
+        // Salva pessoa informada pelo usuário logado
         Pessoa pessoaASalvar = dto.toPessoa();
-        Pessoa pessoaSalva = this.repository.save(pessoaASalvar);
+        Pessoa pessoaSalva = this.pessoaRepository.save(pessoaASalvar);
+
+        // Salva relacionamentos de parentesco, caso tenha sido informado
+        if (dto.parentesco() != null) {
+            Parentesco parentesco = Parentesco.valueOf(dto.parentesco());
+            var usuario = (Usuario) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+            var pessoaLogada = usuario.getPessoa();
+            // salva relacionamento da pessoa cadastrada com o usuário logado
+            this.parentescoPessoasRepository.save(
+                    new ParentescoPessoas(
+                            pessoaLogada,
+                            pessoaSalva,
+                            parentesco)
+            );
+            // Também salva o relacionamento do usuário logado com a pessoa cadastrada
+            this.parentescoPessoasRepository.save(
+                    new ParentescoPessoas(
+                            pessoaSalva,
+                            pessoaLogada,
+                            parentesco.getInversaoDeParentesco(pessoaLogada.getSexo()))
+            );
+
+        }
         return new PessoaDetalheDTO(pessoaSalva);
     }
 
     public PessoaDetalheDTO detalhar(Long id) {
-        Pessoa pessoa = repository.getReferenceById(id);
+        Pessoa pessoa = pessoaRepository.getReferenceById(id);
         return new PessoaDetalheDTO(pessoa);
     }
 }
