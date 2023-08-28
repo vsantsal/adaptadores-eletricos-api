232f76b0767424e4ca2846eabdea55329e23c0b9
vsantsal <vsantsal@gmail.com>
2023-08-28 18:32:24 -0300

    feat: associando endereco cadastrado a usuario logado

--- src/main/java/com/example/adaptadoreseletricos/service/endereco/EnderecoService.java
+++ src/main/java/com/example/adaptadoreseletricos/service/endereco/EnderecoService.java
@@ -1,9 +1,13 @@
 package com.example.adaptadoreseletricos.service.endereco;
 
 import com.example.adaptadoreseletricos.domain.entity.endereco.Endereco;
+import com.example.adaptadoreseletricos.domain.entity.endereco.EnderecosPessoas;
+import com.example.adaptadoreseletricos.domain.entity.pessoa.Pessoa;
 import com.example.adaptadoreseletricos.domain.repository.endereco.EnderecoRepository;
+import com.example.adaptadoreseletricos.domain.repository.endereco.EnderecosPessoasRepository;
 import com.example.adaptadoreseletricos.dto.endereco.EnderecoCadastroDTO;
 import com.example.adaptadoreseletricos.dto.endereco.EnderecoDetalheDTO;
+import com.example.adaptadoreseletricos.service.pessoa.RegistroUsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,17 +16,29 @@ import org.springframework.transaction.annotation.Transactional;
 public class EnderecoService {
 
     @Autowired
-    private EnderecoRepository repository;
+    private EnderecoRepository enderecoRepository;
+
+    @Autowired
+    private EnderecosPessoasRepository enderecosPessoasRepository;
 
     @Transactional
     public EnderecoDetalheDTO salvar(EnderecoCadastroDTO dto) {
+
+        // Salva endereço informado pelo usuário
         Endereco enderecoASalvar = dto.toEndereco();
-        Endereco enderecoSalvo = this.repository.save(enderecoASalvar);
+        Endereco enderecoSalvo = this.enderecoRepository.save(enderecoASalvar);
+
+        // Salva associação de endereço cadastrado ao usuário logado
+        Pessoa pessoa = RegistroUsuarioService.getPessoaLogada();
+        this.enderecosPessoasRepository.save(
+                new EnderecosPessoas(pessoa, enderecoSalvo)
+        );
+
         return new EnderecoDetalheDTO(enderecoSalvo);
     }
 
     public EnderecoDetalheDTO detalhar(Long id) {
-        Endereco endereco = repository.getReferenceById(id);
+        Endereco endereco = enderecoRepository.getReferenceById(id);
         return new EnderecoDetalheDTO(endereco);
     }
 }
