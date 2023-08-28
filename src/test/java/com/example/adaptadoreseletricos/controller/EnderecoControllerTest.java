0a0b26866219fdd8818ceca367c52246e3070f77
vsantsal <vsantsal@gmail.com>
2023-08-28 18:31:56 -0300

    tests: substituicao de mocks por repositorios em memoria

--- src/test/java/com/example/adaptadoreseletricos/controller/EnderecoControllerTest.java
+++ src/test/java/com/example/adaptadoreseletricos/controller/EnderecoControllerTest.java
@@ -2,27 +2,29 @@ package com.example.adaptadoreseletricos.controller;
 
 import com.example.adaptadoreseletricos.domain.entity.endereco.Endereco;
 import com.example.adaptadoreseletricos.domain.entity.endereco.Estado;
+import com.example.adaptadoreseletricos.domain.entity.pessoa.Pessoa;
+import com.example.adaptadoreseletricos.domain.entity.pessoa.Sexo;
+import com.example.adaptadoreseletricos.domain.entity.pessoa.Usuario;
 import com.example.adaptadoreseletricos.domain.repository.endereco.EnderecoRepository;
+import com.example.adaptadoreseletricos.domain.repository.endereco.EnderecosPessoasRepository;
+import com.example.adaptadoreseletricos.domain.repository.pessoa.PessoaRepository;
 import com.example.adaptadoreseletricos.service.endereco.EnderecoService;
-import jakarta.persistence.EntityNotFoundException;
 import org.hamcrest.Matchers;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.boot.test.mock.mockito.SpyBean;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.MediaType;
-import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 
+import java.time.LocalDate;
+
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
@@ -30,37 +32,54 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @ActiveProfiles("test")
 @SpringBootTest
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 @AutoConfigureMockMvc
 class EnderecoControllerTest {
+
+    private final String ENDPOINT = "/enderecos";
     @Autowired
     private MockMvc mockMvc;
 
-    @SpyBean
+    @Autowired
     private EnderecoService service;
 
-    @MockBean
-    private EnderecoRepository repository;
+    @Autowired
+    private PessoaRepository pessoaRepository;
+
+    @Autowired
+    private EnderecoRepository enderecoRepository;
+
+    @Autowired
+    private EnderecosPessoasRepository enderecosPessoasRepository;
+
+    private final Usuario usuario = new Usuario(
+            "usuarioTester",
+            "usuarioTester",
+            new Pessoa(1L,
+                    "Usuario Tester",
+                    LocalDate.of(1981, 1, 1),
+                    Sexo.FEMININO)
+    );
+
+    @BeforeEach
+    public void setUp(){
+        pessoaRepository.save(usuario.getPessoa());
+    }
+
+    @AfterEach
+    public void tearDown(){
+        enderecosPessoasRepository.deleteAll();
+        enderecoRepository.deleteAll();
+        pessoaRepository.deleteAll();
+    }
 
     @DisplayName("Teste de cadastro de endereço válido na API")
-    @WithMockUser(username = "tester")
     @Test
     public void test_deve_criar_endereco_se_dados_informados_validos() throws Exception {
-        // Arrange
-        when(repository.save(any(Endereco.class))).thenReturn(
-                new Endereco(
-                        1L,
-                        "Rua Nascimento Silva",
-                        107L,
-                        "Ipanema",
-                        "Rio de Janeiro",
-                        Estado.RJ
-                )
-        );
-
         // Act
         this.mockMvc.perform(
-                post("/enderecos")
-                        .with(csrf())
+                post(ENDPOINT)
+                        .with(user(usuario))
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(
                                 "{\"rua\": \"Rua Nascimento Silva\", " +
@@ -77,13 +96,12 @@ class EnderecoControllerTest {
     }
 
     @DisplayName("Teste de inclusão de endereço com estado inválido")
-    @WithMockUser(username = "tester")
     @Test
     public void test_deve_informar_erro_requisicao_cliente_se_estado_invalido() throws Exception {
         // Arrange/Act
         this.mockMvc.perform(
-                post("/enderecos")
-                        .with(csrf())
+                post(ENDPOINT)
+                        .with(user(usuario))
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(
                                 "{\"rua\": \"Rua Nascimento Silva\", " +
@@ -97,38 +115,11 @@ class EnderecoControllerTest {
                 .andExpect(status().is4xxClientError());
     }
 
-    @DisplayName("Teste com erro de integridade de dados no BD")
-    @WithMockUser(username = "tester")
-    @Test
-    public void test_deve_informar_erro_requisicao_cliente_se_provoca_erro_integridade_dados() throws Exception {
-        // Arrange
-        when(repository.save(any(Endereco.class))).thenThrow(
-                DataIntegrityViolationException.class
-        );
-
-        // Act
-        this.mockMvc.perform(
-                post("/enderecos")
-                        .with(csrf())
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(
-                                "{\"rua\": \"Rua Nascimento Silva\", " +
-                                        "\"numero\": 107, " +
-                                        "\"bairro\": \"Ipanema\", " +
-                                        "\"cidade\": \"Rio de Janeiro\", " +
-                                        "\"estado\": \"RJ\"}"
-                        )
-        )
-                // Assert
-                .andExpect(status().isConflict());
-    }
-
     @DisplayName("Teste de detalhamento de endereço para id válido na API")
-    @WithMockUser(username = "tester")
     @Test
     public void test_deve_detalhar_endereco_para_id_valido() throws Exception {
         // Arrange
-        when(repository.getReferenceById(1L)).thenReturn(
+        enderecoRepository.save(
                 new Endereco(
                         1L,
                         "Rua Nascimento Silva",
@@ -140,8 +131,8 @@ class EnderecoControllerTest {
         );
 
         // Act
-        this.mockMvc.perform(get("/enderecos/1")
-                .with(csrf()))
+        this.mockMvc.perform(get(ENDPOINT + "/1")
+                .with(user(usuario)))
             // Assert
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.id",
@@ -158,18 +149,13 @@ class EnderecoControllerTest {
                         Matchers.is("RJ")));
     }
 
+
     @DisplayName("Teste de detalhamento de endereco para Id inexistente na API")
-    @WithMockUser(username = "tester")
     @Test
     public void test_nao_deve_detalhar_endereco_para_id_invalido() throws Exception {
-        // Arrange
-        when(repository.getReferenceById(2L)).thenThrow(
-                EntityNotFoundException.class
-        );
-
         // Act
-        this.mockMvc.perform(get("/enderecos/2")
-                .with(csrf()))
+        this.mockMvc.perform(get(ENDPOINT + "/1")
+                        .with(user(usuario)))
 
             // Assert
             .andExpect(status().isNotFound());
@@ -178,13 +164,12 @@ class EnderecoControllerTest {
     }
 
     @DisplayName("Teste de inclusão de endereço com rua muito comprida retorna erro")
-    @WithMockUser(username = "tester")
     @Test
     public void test_deve_informar_erro_requisicao_cliente_se_nome_rua_muito_comprida() throws Exception {
         // Arrange/Act
         this.mockMvc.perform(
-                        post("/enderecos")
-                                .with(csrf())
+                        post(ENDPOINT)
+                                .with(user(usuario))
                                 .contentType(MediaType.APPLICATION_JSON)
                                 .content(
                                         "{\"rua\": \""+ "x".repeat(121)+"\", " +
@@ -199,13 +184,12 @@ class EnderecoControllerTest {
     }
 
     @DisplayName("Teste de inclusão de endereço com rua com número negativo retorna erro")
-    @WithMockUser(username = "tester")
     @Test
     public void test_deve_informar_erro_requisicao_cliente_se_numero_negativo() throws Exception {
         // Arrange/Act
         this.mockMvc.perform(
-                        post("/enderecos")
-                                .with(csrf())
+                        post(ENDPOINT)
+                                .with(user(usuario))
                                 .contentType(MediaType.APPLICATION_JSON)
                                 .content(
                                         "{\"rua\": \"Rua Nascimento Silva\", " +
@@ -219,15 +203,13 @@ class EnderecoControllerTest {
                 .andExpect(status().is4xxClientError());
     }
 
-    @Disabled("WIP: será reavaliado quando voltarmos ao endpoint")
     @DisplayName("Teste de inclusão de endereço com sigla de estado muito comprido retorna erro")
-    @WithMockUser(username = "tester")
     @Test
     public void test_deve_informar_erro_requisicao_cliente_se_sigla_estado_muito_comprido() throws Exception {
         // Arrange/Act
         this.mockMvc.perform(
-                        post("/enderecos")
-                                .with(csrf())
+                        post(ENDPOINT)
+                                .with(user(usuario))
                                 .contentType(MediaType.APPLICATION_JSON)
                                 .content(
                                         "{\"rua\": \"Rua Nascimento Silva\", " +
